2c3d9b89c39f09f225516bba9d34dac2a680606c
Andrew Silva <andrewsilva9@gmail.com>
2019-11-11 02:18:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+  
+  get "/" do
+  end 
+  
+  post "/teams" do
+  end 
+  
+  
 
 
 end
